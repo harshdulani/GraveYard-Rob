912b42362313d305cbda9f4bbcc4c354dded8cde
harshdulani <harshdulani@yahoo.in>
2020-09-04 21:25:41 +0530

    Started Animator Controller setup with 2 packs of mixamo anims

--- Assets/Scripts/PlayerMovementKeyb.cs
+++ Assets/Scripts/PlayerMovementKeyb.cs
@@ -9,16 +9,31 @@ public class PlayerMovementKeyb : MonoBehaviour
 
     private Vector3 direction;
 
+    public float jumpForce = 10f;
+
+    private static Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("isJumping");
+            //GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+        }
 
         if (direction.Equals(Vector3.zero)) //or isn't shooting
             isMoving = false;
         else
         {
             isMoving = true;
-            transform.Translate(direction * Time.deltaTime * movementSpeed, Space.World);
+            transform.Translate(direction * Time.deltaTime * movementSpeed, Space.Self);
         }
     }
 }
